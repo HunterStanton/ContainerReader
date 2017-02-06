d98af83fa153e19b1e34f0a73beb9512fc55bfab
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-06 09:47:07 -0500

    Added usage information

--- ContainerReader/Program.cs
+++ ContainerReader/Program.cs
@@ -10,6 +10,12 @@ namespace ContainerReader
     {
         static void Main(string[] args)
         {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("ContainerReader\nA program that prints infomation about Windows Containers.index files, used to store metadata about configuration/save game files for UWP apps and games.\nUsage: ContainerReader containers.index");
+                return;
+            }
+            return;
         }
     }
 }
